367b7f6f883778bd2b8a21fd84cd341a9c9b65bb
ljurk <ljurk@pm.me>
2019-02-07 17:42:27 +0100

    remove useless stuff

--- dmenutrello/dmenutrello.py
+++ dmenutrello/dmenutrello.py
@@ -10,14 +10,12 @@ LISTS = 1
 CARDS = 2
 COMMENTS = 3
 
-dmenu_show= functools.partial(dmenu.show, font='DejaVu Sans Mono for Powerline-14', background_selected='#2aa198',foreground_selected='#191919', foreground='#2aa198', background='#191919')
+dmenu_show = functools.partial(dmenu.show, font='DejaVu Sans Mono for Powerline-14', background_selected='#2aa198',foreground_selected='#191919', foreground='#2aa198', background='#191919')
 
 def show(mode, data, parent, prompt):
     out = dmenu_show(data.keys(), prompt=prompt)
-    board = None
     #check for match
     if out in data:
-        match = True
         #save object for return, 'this' is the parent of the underlying object
         this = data[out]
         #override match with new dictionary and fill it
@@ -45,20 +43,18 @@ def show(mode, data, parent, prompt):
         return show(mode, data, parent, "ok")
 
 def menu(key, token):
-    client = TrelloClient(
+    parent = TrelloClient(
         api_key = key,
         api_secret = token
         )
-    match = False
 
     data = {}
-    matchedData = None
     #initial filling
-    for board in client.list_boards():
+    for board in parent.list_boards():
         data[board.name] = board
 
     #BOARDS
-    data, parent = show(BOARDS, data, client, '')
+    data, parent = show(BOARDS, data, parent, '')
     #LISTS
     data, parent  = show(LISTS, data, parent, '')
     #CARDS
